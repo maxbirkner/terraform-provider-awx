597d2ef7b1b81461f2246cd165c0655870604953
Quentin Leclerc <quentin.leclerc63@gmail.com>
2024-05-29 15:08:57 +0200

    Fixing credential type permadiff with json content for inputs/injectors

--- internal/awx/resource_credential_type.go
+++ internal/awx/resource_credential_type.go
@@ -1,14 +1,14 @@
 package awx
 
 import (
-	"context"
-	"encoding/json"
-	"strconv"
+    "context"
+    "encoding/json"
+    "strconv"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	awx "github.com/josh-silvas/terraform-provider-awx/tools/goawx"
-	"github.com/josh-silvas/terraform-provider-awx/tools/utils"
+    "github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+    "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+    awx "github.com/josh-silvas/terraform-provider-awx/tools/goawx"
+    "github.com/josh-silvas/terraform-provider-awx/tools/utils"
 )
 
 func resourceCredentialType() *schema.Resource {
@@ -102,10 +102,20 @@ func resourceCredentialTypeRead(_ context.Context, d *schema.ResourceData, m int
 	if err := d.Set("kind", credType.Kind); err != nil {
 		return diag.FromErr(err)
 	}
-	if err := d.Set("inputs", credType.Inputs); err != nil {
-		return diag.FromErr(err)
+
+    inputsBytes, err := json.Marshal(credType.Inputs)
+    if err != nil {
+        return diag.FromErr(err)
+    }
+    injectorBytes, err := json.Marshal(credType.Injectors)
+    if err != nil {
+        return diag.FromErr(err)
+    }
+
+	if err := d.Set("inputs", string(inputsBytes)); err != nil {
+        return diag.FromErr(err)
 	}
-	if err := d.Set("injectors", credType.Injectors); err != nil {
+	if err := d.Set("injectors", string(injectorBytes)); err != nil {
 		return diag.FromErr(err)
 	}
 
