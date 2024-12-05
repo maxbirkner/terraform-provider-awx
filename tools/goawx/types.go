96cec6d798753f9270856b3460b45464d9a38972
Oleh Khalin <oleghalinua@gmail.com>
2024-12-05 15:59:36 +0100

    fix: User.Type

--- tools/goawx/types.go
+++ tools/goawx/types.go
@@ -688,7 +688,7 @@ type JobEvent struct {
 // User represents an user.
 type User struct {
 	ID              int         `json:"id"`
-	Type            int         `json:"string"`
+	Type            string      `json:"string"`
 	URL             string      `json:"url"`
 	Related         *Related    `json:"related"`
 	SummaryFields   *Summary    `json:"summary_fields"`
